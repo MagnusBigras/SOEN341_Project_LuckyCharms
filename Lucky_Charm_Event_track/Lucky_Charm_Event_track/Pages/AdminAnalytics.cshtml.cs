9042ae2d7ab3b3d6e99eec5d3fc0916024bf62aa
Alexandre Nasseri <a_nasseri2@yahoo.ca>
2025-10-15 16:57:11 -0400

    removed events page in student portal. we can already see the details when we click on the event in the calendar. #73

---
+++ Lucky_Charm_Event_track/Lucky_Charm_Event_track/Pages/AdminAnalytics.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lucky_Charm_Event_track.Pages
+{
+    public class AdminEventAnalyticsModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Empty for now
+        }
+    }
+}
