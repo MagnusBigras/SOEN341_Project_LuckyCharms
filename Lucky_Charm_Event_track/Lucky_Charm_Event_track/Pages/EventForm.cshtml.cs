d55faa6f355c81dbe4b4e028b255db6fe2112531
Alexandre Nasseri <a_nasseri2@yahoo.ca>
2025-10-18 18:58:01 -0400

    made event form confirmation message as well as added some metrics for the event form.

--- Lucky_Charm_Event_track/Lucky_Charm_Event_track/Pages/EventForm.cshtml.cs
+++ Lucky_Charm_Event_track/Lucky_Charm_Event_track/Pages/EventForm.cshtml.cs
@@ -5,19 +5,47 @@ namespace Lucky_Charm_Event_track.Pages
 {
     public class EventFormModel : PageModel
     {
-        // Add properties to bind form data
         [BindProperty]
         public string EventName { get; set; }
+
         [BindProperty]
         public string EventDate { get; set; }
+
         [BindProperty]
         public string EventTime { get; set; }
+
         [BindProperty]
         public string EventLocation { get; set; }
+
+        [BindProperty]
+        public string Address { get; set; }
+
+        [BindProperty]
+        public string City { get; set; }
+
+        [BindProperty]
+        public string Region { get; set; }
+
+        [BindProperty]
+        public string PostalCode { get; set; }
+
+        [BindProperty]
+        public string Country { get; set; }
+
+        [BindProperty]
+        public int Capacity { get; set; }
+
+        [BindProperty]
+        public string EventType { get; set; } // "paid" or "free"
+
+        [BindProperty]
+        public decimal Price { get; set; }
+
+        [BindProperty]
+        public string Category { get; set; } 
+
         [BindProperty]
         public string EventDescription { get; set; }
-        [BindProperty]
-        public string EventType { get; set; }
 
         public void OnGet()
         {
