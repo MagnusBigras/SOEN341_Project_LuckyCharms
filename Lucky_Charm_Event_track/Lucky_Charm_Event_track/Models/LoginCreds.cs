a8e295bdea4789df36589f83516982853dd11462
Magnus Bigras <magnus.bigras@videotron.ca>
2025-10-25 11:00:59 -0400

    Fixed Api call for Login
    Work done:
    -Added api login call
    -updated login script

---
+++ Lucky_Charm_Event_track/Lucky_Charm_Event_track/Models/LoginCreds.cs
@@ -0,0 +1,8 @@
+﻿namespace Lucky_Charm_Event_track.Models
+{
+    public class LoginCreds
+    {
+        public string Username { get; set; }    
+        public string Password { get; set; }
+    }
+}
