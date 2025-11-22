65b5b0e90c61b8170536c5a74eec0583da34b5ac
Alexandre Nasseri <a_nasseri2@yahoo.ca>
2025-11-22 08:08:30 -0500

    Fix event capacity and favorites management

---
+++ Lucky_Charm_Event_track/Lucky_Charm_Event_track/Migrations/20251122044445_AddFavouriteEventsColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LuckyCharmEventtrack.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFavouriteEventsColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FavouriteEventIds",
+                table: "UserAccounts",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FavouriteEventIds",
+                table: "UserAccounts");
+        }
+    }
+}
